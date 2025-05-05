47357428d603cad0abcdc65979f154ade85e20d5
zohaib-asghar-devexelit <zohaib.ali@devexcelit.com>
2025-05-05 15:01:24 +0500

    changing to lambda

--- src/main.ts
+++ src/main.ts
@@ -1,39 +1,19 @@
-import { Callback, Context, Handler } from 'aws-lambda';
-import { createServer, proxy } from 'aws-serverless-express';
-import express from 'express';
 import { NestFactory } from '@nestjs/core';
-import { ExpressAdapter } from '@nestjs/platform-express';
 import { AppModule } from './app.module';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
-let cachedServer;
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
 
-const bootstrapServer = async () => {
-  if (!cachedServer) {
-    const expressApp = express();
-    const adapter = new ExpressAdapter(expressApp);
-    const app = await NestFactory.create(AppModule, adapter);
+  const config = new DocumentBuilder()
+    .setTitle('Company Auth API')
+    .setDescription('API for registering and authenticating companies')
+    .setVersion('1.0')
+    .build();
 
-    const config = new DocumentBuilder()
-      .setTitle('Company Auth API')
-      .setDescription('API for registering and authenticating companies')
-      .setVersion('1.0')
-      .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document); // Swagger UI at /api
 
-    const document = SwaggerModule.createDocument(app, config);
-    SwaggerModule.setup('api', app, document);
-
-    await app.init();
-    cachedServer = createServer(expressApp);
-  }
-  return cachedServer;
-};
-
-export const handler: Handler = async (
-  event: any,
-  context: Context,
-  callback: Callback,
-) => {
-  const server = await bootstrapServer();
-  return proxy(server, event, context, 'PROMISE').promise;
-};
+  await app.listen(process.env.PORT ?? 3333);
+}
+bootstrap();

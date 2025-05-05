3ffaeef64aedb3ea41dbbe00fad77f442ab6dc6a
zohaib-asghar-devexelit <zohaib.ali@devexcelit.com>
2025-05-05 15:19:27 +0500

    uodate main.ts

--- src/main.ts
+++ src/main.ts
@@ -3,33 +3,22 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ExpressAdapter } from '@nestjs/platform-express';
-import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
-import * as express from 'express';
-import { Server } from 'http';
+import express from 'express';
+
 
 const server = express();
 
-let cachedApp: Server;
+let appInitialized: boolean = false;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, new ExpressAdapter(server));
-
-  const config = new DocumentBuilder()
-    .setTitle('Company Auth API')
-    .setDescription('API for registering and authenticating companies')
-    .setVersion('1.0')
-    .build();
-
-  const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api', app, document);
-
   await app.init();
-  return server;
+  appInitialized = true;
 }
 
 export default async function handler(req: any, res: any) {
-  if (!cachedApp) {
-    cachedApp = await bootstrap();
+  if (!appInitialized) {
+    await bootstrap();
   }
-  return cachedApp(req, res);
+  return server(req, res);
 }

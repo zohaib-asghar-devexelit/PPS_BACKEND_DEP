e1f26d1d31ea4e2c821b8ff49bcac50cf3105692
zohaib-asghar-devexelit <zohaib.ali@devexcelit.com>
2025-05-05 15:53:03 +0500

    testing deployement

--- src/main.ts
+++ src/main.ts
@@ -1,34 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { ExpressAdapter } from '@nestjs/platform-express';
-import express from 'express';
-
-const expressApp = express();
-let app;
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
-  if (!app) {
-    app = await NestFactory.create(AppModule, new ExpressAdapter(expressApp));
-    await app.init();
-  }
-  return app;
+  const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Company Auth API')
+    .setDescription('API for registering and authenticating companies')
+    .setVersion('1.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document); // Swagger UI at /api
+
+  await app.listen(process.env.PORT ?? 3333);
 }
-
-// For local development
-if (process.env.NODE_ENV !== 'production') {
-  bootstrap().then((app) => {
-    const port = process.env.PORT || 3002;
-    app.getHttpAdapter().getInstance().listen(port, () => {
-      console.log(`Server running on http://localhost:${port}`);
-    });
-  });
-}
-
-// For Vercel
-export const handler = async (req: any, res: any) => {
-  const app = await bootstrap();
-  const instance = app.getHttpAdapter().getInstance();
-  instance(req, res); // ✅ Express instance handles the request
-};
-
-export default handler;
+bootstrap();
